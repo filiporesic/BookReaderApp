af0b1ab3b7e0a6a2a5b0eb1db38486fa9fc927f1
dmaduna <dinko.maduna@gmail.com>
2024-06-12 22:50:15 +0200

    dodane knjige u solution

--- Forms/FrontPage.designer.cs
+++ Forms/FrontPage.designer.cs
@@ -35,8 +35,8 @@
             this.borrowBooksGridView = new System.Windows.Forms.DataGridView();
             this.genreComboBox = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.borrowButton = new System.Windows.Forms.Button();
             this.walletBalance = new System.Windows.Forms.Label();
+            this.borrowButton = new System.Windows.Forms.Button();
             this.costLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.borrowBooksGridView)).BeginInit();
             this.SuspendLayout();
@@ -45,10 +45,10 @@
             // 
             this.WalletButton.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.WalletButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.WalletButton.Location = new System.Drawing.Point(1080, 479);
-            this.WalletButton.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.WalletButton.Location = new System.Drawing.Point(1620, 737);
+            this.WalletButton.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.WalletButton.Name = "WalletButton";
-            this.WalletButton.Size = new System.Drawing.Size(146, 42);
+            this.WalletButton.Size = new System.Drawing.Size(219, 65);
             this.WalletButton.TabIndex = 26;
             this.WalletButton.Text = "Wallet";
             this.WalletButton.UseVisualStyleBackColor = false;
@@ -58,10 +58,10 @@
             // 
             this.BorrowedButton.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.BorrowedButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BorrowedButton.Location = new System.Drawing.Point(860, 479);
-            this.BorrowedButton.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.BorrowedButton.Location = new System.Drawing.Point(1290, 737);
+            this.BorrowedButton.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.BorrowedButton.Name = "BorrowedButton";
-            this.BorrowedButton.Size = new System.Drawing.Size(146, 42);
+            this.BorrowedButton.Size = new System.Drawing.Size(219, 65);
             this.BorrowedButton.TabIndex = 27;
             this.BorrowedButton.Text = "Borrowed books";
             this.BorrowedButton.UseVisualStyleBackColor = false;
@@ -72,8 +72,7 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(33, 31);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(50, 48);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(518, 22);
             this.label1.TabIndex = 35;
@@ -89,14 +88,14 @@
             this.borrowBooksGridView.BackgroundColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.borrowBooksGridView.ColumnHeadersHeight = 28;
             this.borrowBooksGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-            this.borrowBooksGridView.Location = new System.Drawing.Point(36, 70);
-            this.borrowBooksGridView.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.borrowBooksGridView.Location = new System.Drawing.Point(54, 108);
+            this.borrowBooksGridView.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.borrowBooksGridView.Name = "borrowBooksGridView";
             this.borrowBooksGridView.ReadOnly = true;
             this.borrowBooksGridView.RowHeadersVisible = false;
             this.borrowBooksGridView.RowHeadersWidth = 62;
             this.borrowBooksGridView.RowTemplate.Height = 28;
-            this.borrowBooksGridView.Size = new System.Drawing.Size(1190, 338);
+            this.borrowBooksGridView.Size = new System.Drawing.Size(1785, 520);
             this.borrowBooksGridView.TabIndex = 34;
             this.borrowBooksGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.BorrowBooksGridView_CellClick);
             this.borrowBooksGridView.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.BorrowBooksGridView_CellDoubleClick);
@@ -104,10 +103,9 @@
             // genreComboBox
             // 
             this.genreComboBox.FormattingEnabled = true;
-            this.genreComboBox.Location = new System.Drawing.Point(1080, 32);
-            this.genreComboBox.Margin = new System.Windows.Forms.Padding(2);
+            this.genreComboBox.Location = new System.Drawing.Point(1620, 49);
             this.genreComboBox.Name = "genreComboBox";
-            this.genreComboBox.Size = new System.Drawing.Size(146, 21);
+            this.genreComboBox.Size = new System.Drawing.Size(217, 28);
             this.genreComboBox.TabIndex = 36;
             this.genreComboBox.SelectedIndexChanged += new System.EventHandler(this.genreComboBox_SelectedIndexChanged);
             // 
@@ -116,25 +114,11 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(877, 32);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(1316, 49);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(199, 22);
             this.label2.TabIndex = 37;
             this.label2.Text = "Filer books by genre:";
-            // 
-            // borrowButton
-            // 
-            this.borrowButton.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.borrowButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.borrowButton.Location = new System.Drawing.Point(36, 479);
-            this.borrowButton.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
-            this.borrowButton.Name = "borrowButton";
-            this.borrowButton.Size = new System.Drawing.Size(111, 41);
-            this.borrowButton.TabIndex = 38;
-            this.borrowButton.Text = "BORROW";
-            this.borrowButton.UseVisualStyleBackColor = false;
-            this.borrowButton.Click += new System.EventHandler(this.BorrowButton_Click);
             // 
             // walletBalance
             // 
@@ -143,12 +127,24 @@
             this.walletBalance.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.walletBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.walletBalance.ForeColor = System.Drawing.Color.White;
-            this.walletBalance.Location = new System.Drawing.Point(1042, 423);
-            this.walletBalance.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.walletBalance.Location = new System.Drawing.Point(1563, 651);
             this.walletBalance.Name = "walletBalance";
             this.walletBalance.Size = new System.Drawing.Size(156, 24);
             this.walletBalance.TabIndex = 39;
             this.walletBalance.Text = "Wallet balance: ";
+            // 
+            // borrowButton
+            // 
+            this.borrowButton.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.borrowButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.borrowButton.Location = new System.Drawing.Point(54, 737);
+            this.borrowButton.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.borrowButton.Name = "borrowButton";
+            this.borrowButton.Size = new System.Drawing.Size(166, 63);
+            this.borrowButton.TabIndex = 38;
+            this.borrowButton.Text = "BORROW";
+            this.borrowButton.UseVisualStyleBackColor = false;
+            this.borrowButton.Click += new System.EventHandler(this.BorrowButton_Click);
             // 
             // costLabel
             // 
@@ -156,8 +152,7 @@
             this.costLabel.BackColor = System.Drawing.Color.Transparent;
             this.costLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.costLabel.ForeColor = System.Drawing.Color.White;
-            this.costLabel.Location = new System.Drawing.Point(33, 423);
-            this.costLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.costLabel.Location = new System.Drawing.Point(50, 651);
             this.costLabel.Name = "costLabel";
             this.costLabel.Size = new System.Drawing.Size(74, 22);
             this.costLabel.TabIndex = 40;
@@ -165,12 +160,12 @@
             // 
             // FrontPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.BackgroundImage = global::BookReaderApp.Properties.Resources.background_login;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1265, 611);
+            this.ClientSize = new System.Drawing.Size(1898, 940);
             this.Controls.Add(this.costLabel);
             this.Controls.Add(this.walletBalance);
             this.Controls.Add(this.borrowButton);
@@ -181,7 +176,7 @@
             this.Controls.Add(this.BorrowedButton);
             this.Controls.Add(this.WalletButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.Name = "FrontPage";
             this.Text = "Front Page";
             this.Load += new System.EventHandler(this.FrontPageV2_Load);
@@ -198,8 +193,8 @@
         private System.Windows.Forms.DataGridView borrowBooksGridView;
         private System.Windows.Forms.ComboBox genreComboBox;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button borrowButton;
         private System.Windows.Forms.Label walletBalance;
+        private System.Windows.Forms.Button borrowButton;
         private System.Windows.Forms.Label costLabel;
     }
 }
